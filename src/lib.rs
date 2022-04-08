1ede2659cef2e2e4994acd9c9271bc5d3cd6f500
Erik Tesar <erik@erik-tesar.com>
2022-04-08 15:01:49 +0200

    chore: init

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+//! To be written
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    explicit_outlives_requirements,
+    clippy::missing_const_for_fn,
+    clippy::missing_errors_doc
+)]
+#![deny(
+    rustdoc::broken_intra_doc_links,
+    rustdoc::bare_urls,
+    macro_use_extern_crate,
+    non_ascii_idents,
+    elided_lifetimes_in_paths
+)]
+#![forbid(unsafe_code)]
+#![no_std]
