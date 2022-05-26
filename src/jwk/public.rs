2178ea042d93cbd82343e662e7229e33f061b0c6
Erik Tesar <erik@erik-tesar.com>
2022-05-26 13:25:42 +0200

    fix: public should definitely not store the private part of an rsa key ...

--- src/jwk/public.rs
+++ src/jwk/public.rs
@@ -1,9 +1,11 @@
-use super::{ec::EcPublic, okp::OkpPublic, rsa::RsaPrivateKey};
+use super::{ec::EcPublic, okp::OkpPublic, rsa::RsaPublicKey};
 
+/// The `public` part of some asymmetric cryptographic key
 #[non_exhaustive]
 #[derive(Debug)]
 pub enum Public {
-    Rsa(RsaPrivateKey),
+    /// The public part of a
+    Rsa(RsaPublicKey),
     Ec(EcPublic),
     Okp(OkpPublic),
 }
